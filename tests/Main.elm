84dfe94233a3eee0d9eeb537f9c1bd432aafbb56
Bogdan Popa <popa.bogdanp@gmail.com>
2016-11-17 12:18:25 +0200

    bump version

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,13 +1,25 @@
 port module Main exposing (..)
 
 import Json.Encode exposing (Value)
+import Test exposing (Test, describe)
 import Test.Runner.Node exposing (TestProgram, run)
-import Tests
+import TestDate
+import TestDateTime
+import TestZonedDateTime
 
 
 main : TestProgram
 main =
-    run emit Tests.all
+    run emit all
+
+
+all : Test
+all =
+    describe "elm-utc"
+        [ TestDate.all
+        , TestDateTime.all
+        , TestZonedDateTime.all
+        ]
 
 
 port emit : ( String, Value ) -> Cmd msg
