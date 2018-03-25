5926ece91b8f5bfe2e97d3f3c4e44ecbf3eb3e36
Scott Smith <scottnelsonsmith@gmail.com>
2018-03-24 21:45:49 -0700

    Starting to convert to style-attributes

--- examples/with-parser-error-renderer/Main.elm
+++ examples/with-parser-error-renderer/Main.elm
@@ -10,18 +10,77 @@ the `elm-time` library.
 
 -}
 
-import Html exposing (Html, pre, text)
-import Time.DateTime exposing (fromISO8601)
-import Time.ISO8601Error exposing (renderText, reflow)
+import Color
+import Element exposing (..)
+import Element.Attributes exposing(..)
+import Html exposing (Html)
+import Parser
+import Style exposing (..)
+import Style.Border as Border
+import Style.Color as Color
+import Style.Font as Font
+import Style.Transition as Transition
+import Time exposing (Time, every)
+import Time.DateTime
+    exposing
+        ( DateTime
+        , fromISO8601
+        , fromTimestamp
+        , fromTuple
+        , toISO8601
+        )
+import Time.ISO8601Error exposing (reflow, renderText)
+
+
+type Msg
+    = Tick Time
 
 
 {-| The "entry"
 -}
-main : Html msg
 main =
-    case fromISO8601 "1991-02-29T12:25:12.0Z" of
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { iso8601input : String
+    , result : DateTime
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { iso8601input = "1991-02-28T12:25:12.0Z"
+      , result = fromTuple ( 1991, 2, 28, 12, 15, 12, 0 )
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick time ->
+            ( { model
+                | iso8601input =
+                    time
+                        |> fromTimestamp
+                        |> toISO8601
+              }
+            , Cmd.none
+            )
+
+
+view : Model -> Html Msg
+view model =
+    case fromISO8601 model.iso8601input of
         Ok v ->
-            text <| toString v
+            Html.text <| toString v
 
         Err e ->
             let
@@ -30,4 +89,9 @@ main =
                         ++ "\n\n"
                         ++ reflow (toString e)
             in
-            Html.pre [] [ text <| msg ]
+            Html.pre [] [ Html.text <| msg ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    every Time.second Tick
