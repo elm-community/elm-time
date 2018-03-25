e695e231855da5b9622e091bfa14bb3fe4c2ee2e
Scott Smith <scottnelsonsmith@gmail.com>
2018-03-24 17:10:51 -0700

    Setting up full example elm-app

--- src/Time/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-(("TimeZoneData.elm" . ((nil . ((elm-format-on-save . nil)
-                                (elm-tags-on-save . nil)))))
- ("TimeZones.elm" . ((nil . ((elm-format-on-save . nil)
-                             (elm-tags-on-save . nil))))))
